91ef0041f8fca68a3c8b1059f6201a22f1f20404
Kelly Marchisio <kellymarchisio@dhcp-10-248-118-162.eduroam.wireless.private.cam.ac.uk>
2018-02-24 16:44:16 +0000

    revert accidental commit

--- groundhog/layers/cost_layers.py
+++ groundhog/layers/cost_layers.py
@@ -952,11 +952,6 @@ class SoftmaxLayer(CostLayer):
                 print "".join(sen),
             else:
                 for k in xrange(values.shape[0]):
-                    print 'cost_layers'
-                    print model.word_indxs
-                    print values
-                    print k
-                    print model.word_indxs[values[k]],
                     if model.word_indxs[values[k]] == '<eol>':
                         break
             print
